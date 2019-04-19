e1ef12c15cbfb99ecb81f576b07b6a85b8c2887e
HUi <huynguyeexn@github.com>
2019-04-19 23:04:47 +0700

    add section heading

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+	require_once 'hn_layout/hn.layout.header';
+	require_once 'hn_layout/hn.layout.carousel';
+	require_once 'hn_layout/hn.layout.features';
+	require_once 'hn_layout/hn.layout.review';
+	require_once 'hn_layout/hn.layout.courses';
+	require_once 'hn_layout/hn.layout.partner';
+	require_once 'hn_layout/hn.layout.getinbox';
+
+
+
+	require_once 'hn_layout/hn.layout.footer';
+?>
